45aa0e6a8af6c0a3b93f11a19f42dc6094305ab1
Juan Camilo Neira Campos <j.neira@uniandes.edu.co>
2023-12-04 11:33:14 -0500

    Update

--- historia_clinica/views.py
+++ historia_clinica/views.py
@@ -5,12 +5,17 @@ from django.views import View
 import json
 
 def historia_clinica_por_documento(request):
-    # Asegúrate de que la solicitud sea de tipo GET
-    if request.method == 'GET':
+    if request.method == 'POST':
         try:
+            # Asegúrate de que el contenido de la solicitud sea de tipo application/json
+            if request.content_type != 'application/json':
+                return JsonResponse({'error': 'Tipo de contenido no admitido'}, status=415)
+
             data = json.loads(request.body.decode('utf-8'))
 
             documento_paciente = data.get('documento_paciente')
+
+            # Aquí deberías validar que el campo 'documento_paciente' existe y tiene el formato correcto
 
             historia_clinica = get_object_or_404(HistoriaClinica, documento_paciente=documento_paciente)
 
@@ -23,12 +28,10 @@ def historia_clinica_por_documento(request):
                 'fecha_creacion': historia_clinica.fecha_creacion.strftime("%Y-%m-%d %H:%M:%S")
             }
 
-            # Devuelve la respuesta JSON
             return JsonResponse(response_data)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Cuerpo de la solicitud no válido'}, status=400)
     else:
-        # Si la solicitud no es de tipo GET, devuelve un error o una respuesta adecuada
         return JsonResponse({'error': 'Método no permitido'}, status=405)
 
 class crear_historia_clinica(View):
